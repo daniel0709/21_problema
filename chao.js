667c536ae456a884edfd24d1651dda3e1230e2ca
daniel0709 <98121186+daniel0709@users.noreply.github.com>
2022-05-12 20:08:05 -0300

    Add files via upload

---
+++ chao.js
@@ -0,0 +1,12 @@
+class Chao{
+    constructor(x,y,l,h){
+        this.x = x
+        this.y = y
+        this.l = l
+        this.h = h
+        this.body = Bodies.rectangle
+    }
+    display()
+    
+
+} 
